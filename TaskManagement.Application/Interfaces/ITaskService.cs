42bbe27882e2311e48ab732945bff95df117a397
eric-akh <eric7313@yahoo.com>
2025-07-08 14:52:32 +1000

    Add TaskService and ITaskService in Application layer

---
+++ TaskManagement.Application/Interfaces/ITaskService.cs
@@ -0,0 +1,12 @@
+﻿using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Application.Interfaces;
+
+public interface ITaskService
+{
+    Task<IEnumerable<TaskItem>> GetAllTasksAsync();
+    Task<TaskItem?> GetTaskByIdAsync(Guid id);
+    Task CreateTaskAsync(TaskItem task);
+    Task UpdateTaskAsync(TaskItem task);
+    Task DeleteTaskAsync(Guid id);
+}
---
+++ TaskManagement.Application/Services/TaskService.cs
@@ -0,0 +1,40 @@
+﻿using TaskManagement.Application.Interfaces;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Domain.Interfaces;
+
+namespace TaskManagement.Application.Services;
+
+public class TaskService : ITaskService
+{
+    private readonly ITaskRepository _repository;
+
+    public TaskService(ITaskRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<TaskItem>> GetAllTasksAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<TaskItem?> GetTaskByIdAsync(Guid id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task CreateTaskAsync(TaskItem task)
+    {
+        await _repository.AddAsync(task);
+    }
+
+    public async Task UpdateTaskAsync(TaskItem task)
+    {
+        await _repository.UpdateAsync(task);
+    }
+
+    public async Task DeleteTaskAsync(Guid id)
+    {
+        await _repository.DeleteAsync(id);
+    }
+}
