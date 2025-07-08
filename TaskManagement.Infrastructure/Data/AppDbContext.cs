411d979f1b413b8a88a6817303bb1cb12416506b
eric-akh <eric7313@yahoo.com>
2025-07-08 23:51:45 +1000

    Add AppDbContext and TaskRepository with EF Core in Infrastructure layer

---
+++ TaskManagement.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+}
---
+++ TaskManagement.Infrastructure/Data/TaskRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Domain.Interfaces;
+
+namespace TaskManagement.Infrastructure.Data;
+
+public class TaskRepository(AppDbContext context) : ITaskRepository
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task<IEnumerable<TaskItem>> GetAllAsync()
+    {
+        return await _context.Tasks.ToListAsync();
+    }
+
+    public async Task<TaskItem?> GetByIdAsync(Guid id)
+    {
+        return await _context.Tasks.FindAsync(id);
+    }
+
+    public async Task AddAsync(TaskItem task)
+    {
+        await _context.Tasks.AddAsync(task);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(TaskItem task)
+    {
+        _context.Tasks.Update(task);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var task = await _context.Tasks.FindAsync(id);
+        if (task is not null)
+        {
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
